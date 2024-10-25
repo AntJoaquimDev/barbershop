d2d5489ee6ac49659719824d45129ad84a2e8af4
AntJoaquimDev <a.joaquimsfilho@gmail.com>
2024-10-25 12:16:14 -0300

    cretae_anination_in_splashpage

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,22 +1,55 @@
-import 'package:asyncstate/asyncstate.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationOpacityLogo = 0.0;
+
+  double get _logoAnimationWidth=> 100 *_scale;
+  double get _logoAnimationHeight=> 100 *_scale;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale=1.0;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash-Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              await Future.delayed(const Duration(seconds: 2)).asyncLoader();
-            },
-            child: const Text('child')),
-      ),
-    );
+        backgroundColor: Colors.black,
+        body: DecoratedBox(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/background_image_chair.jpg',
+                ),
+                opacity: 0.2,
+                fit: BoxFit.cover),
+          ),
+          child: AnimatedOpacity(
+            duration: Duration(seconds: 3),
+            opacity: _animationOpacityLogo,
+            curve: Curves.easeIn,
+            child: Center(
+              child: AnimatedContainer(
+                width: _logoAnimationWidth,
+                height: _logoAnimationHeight,
+                  duration:const Duration(seconds: 3),
+                  curve: Curves.linearToEaseOut,
+                  child: Image.asset('assets/images/imgLogo.png',fit: BoxFit.cover,)),
+            ),
+          ),
+        ));
   }
 }
